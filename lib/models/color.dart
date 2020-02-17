6b12ec121843fbb2273ed9180833aff6e638d764
fonkamloic <fonkamloic@gmail.com>
2020-02-17 02:32:42 +0100

    color_model

---
+++ lib/models/color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class ColorModel{
+  final double index;
+
+  final Color color;
+  final String name;
+
+  ColorModel({this.name, this.color, this.index});
+}
